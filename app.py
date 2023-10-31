8c9cf099bf31a5f700782025efbe42054a2f5720
kr <karl.rojas@viacesi.fr>
2023-10-31 10:11:16 +0100

    test

---
+++ app.py
@@ -0,0 +1,395 @@
+from shiny import Inputs, Outputs, Session, App, ui, render, reactive
+from pathlib import Path
+from test import treatment_file, data_separation, data_split, LR, evaluate_linear_regression, train_and_evaluate_random_forest, plot_linear_regression_results, create_shap_waterfall_chart, shap_beeswarm_plot, shap_violin_plot, Split_and_Shap,create_and_display_graph_test, Joblib
+from test import plot_RF_results, RF, patient_data, pred_plot
+import shiny.experimental as x
+import json
+import requests_fhir as requests
+import pandas as pd
+from datetime import datetime
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
+from sklearn.impute import SimpleImputer
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.ensemble import RandomForestRegressor
+import shap
+import sklearn
+from shinywidgets import output_widget, register_widget, render_widget
+from io import BytesIO
+import plotly.express as px
+from asyncio import sleep
+import numpy as np
+import plotly.graph_objs as go
+from shiny import App, reactive, ui, Session, render, Inputs, Outputs
+from sklearn.linear_model import LinearRegression
+import base64
+import random
+import shinyswatch
+from htmltools import css
+import seaborn as sns
+from flask import send_file
+import plotly.express as px
+import streamlit as st
+import tempfile
+import os
+import joblib 
+BASE_URL = 'https://test/fhir'
+
+#Opening the Treatment CSV file
+infile = Path(__file__).parent / "data/treat_data.csv"
+treat = pd.read_csv(infile)
+
+#Loading the pre-trained model and Opening the Simulated Data
+model = joblib.load('model.joblib')
+data = pd.read_csv('simulated_data.csv')
+
+
+app_ui = ui.page_fluid(
+    ui.panel_title("AI Dashboard for Cancer Care"), 
+    {"style": "text-align : center;" "font-weight : bold;"},
+    output_widget("my_widget"),
+    ui.panel_main(
+        shinyswatch.theme.darkly(),
+        ui.navset_tab(
+             ui.nav(
+                "Patient Informations",
+                x.ui.card(
+                    x.ui.card_header("Patient Info"),
+                    ui.input_numeric("patient_id", "Enter the Patient ID", 2, min=1, max=1000000000),
+                    ui.p(ui.input_action_button("send", "Enter", class_="btn-primary")),
+                    ui.output_table("patient_table"),
+                ),
+                x.ui.card(
+                    x.ui.card_header("Patient History"),
+                    ui.input_text("snowmed", "Snowmed code"),
+                    ui.p(ui.input_action_button("send2", "Enter", class_="btn-primary")),
+                    ui.output_table("history"),
+                ),
+            ),
+            ui.nav(
+                "Linear Regression & Random Forest",
+                ui.row(
+                    ui.column(
+                        6,
+                        x.ui.card(
+                            x.ui.card_header("Linear Regression "),
+                            ui.output_plot("Data_test"),
+                        ),
+                        x.ui.card(
+                            x.ui.card_header("Linear Regression Waterfall chart"),
+                            ui.output_plot("WaterfallPNG"),
+                        ),
+                    ),
+                    ui.column(
+                        6,
+                        x.ui.card(
+                            x.ui.card_header("Random Forest"),
+                            ui.output_plot("Random_Forest_plot"),
+                        ),
+
+                        x.ui.card(
+                            x.ui.card_header("Waterfall Random Forest"),
+                            ui.output_plot("WaterRF")
+                        ),
+                    ),
+                ),
+            ),
+            ui.nav(
+                "BeeSwarm & Violin Graphs",
+                x.ui.card(
+                    x.ui.card_header("Positive and negative SHAP features"),
+                    ui.output_text("positive_negative"),
+                    x.ui.card_header("BeeSwarm"),
+                    ui.output_plot("plot_bee"),
+                ),
+                x.ui.card(
+                    x.ui.card_header("Violin Chart"),
+                    ui.output_plot("plot_violin")
+                ),
+            ),
+            ui.nav(
+                "What if analysis",
+                    x.ui.card(
+                    x.ui.card_header("What if"),
+                    ui.p(ui.input_action_button("pred", "Create a new Prediction!", class_="btn-primary")),
+                    ui.input_slider(
+                        "age",
+                        "Age",
+                        0,
+                        120,
+                        65,
+                    ),
+                    ui.input_slider(
+                        "blood_pressure",
+                        "Blood_Pressure",
+                        60,
+                        150,
+                        100,
+                        step=0.01,
+                        animate = True
+                    ),
+                    ui.input_selectize(
+                        "gender",
+                        "Choose your gender:",
+                        {
+                            "Gender": {"M": "Male", "F": "Female", "O": "Others"},
+                        },
+                        multiple=False,
+                        selected=False,
+                    ),
+                    ui.input_selectize(
+                        "diabetes",
+                        "Diabetes or not:",
+                        {
+                            "Diabetes": {"Y": "Yes", "N": "No"},
+                        },
+                        multiple=False,
+                        selected=False,
+                    ),
+                    ui.output_text("New_Prediction"),
+                ),
+                x.ui.card(
+                    x.ui.card_header("Current plot"),
+                    ui.output_plot("Current"),
+                ),
+                x.ui.card(
+                    x.ui.card_header("What if plot"),
+                    ui.output_plot("new_LR_plt"),
+                ),
+            ),
+            
+            ui.nav(
+                "Joblib Prediction",
+                x.ui.card(
+                    x.ui.card_header("Patient ROW"),
+                    ui.input_numeric("patient_row", "Enter the Patient ROW", 1, min=1, max=len(data)),
+                    ui.p(ui.input_action_button("send3", "Enter", class_="btn-primary")),
+                    ui.output_text("patient_Row"),
+                ),
+                x.ui.card(
+                    x.ui.card_header("Predictions results"),
+                    ui.output_text("Pred"),
+                ),
+                x.ui.card(
+                    x.ui.card_header("Predictions Plot"),
+                    ui.output_plot("Pred_plot"),
+
+                )
+            ),
+           
+            ui.nav(
+                "Treatment Plans",
+                
+            ),
+            ui.nav(
+                "Feedback and Support"
+            )
+        )
+    ),
+)
+
+#Server part of the Shiny for Python code :
+def server(input: Inputs, output: Outputs, session: Session):
+
+#Loading the treat CSV file
+    @output
+    @render.table
+    def Treattable():
+        infile = Path(__file__).parent / "data/treat_data.csv"
+        treat = pd.read_csv(infile)
+        return treat
+    
+#Trying to display the Linear Regression Graphs and Waterfall chart on the Dashboard by creating
+#PNG images and using them to display them on the dashboard
+    @output
+    @render.plot
+    def Data_test():
+        x, y = data_separation(treat)
+        x = pd.DataFrame(x)
+        x = x.fillna(0)
+        x = x.dropna()
+        X_train, X_test, Y_train, Y_test = data_split(x,y)
+        lr, y_lr_train_pred, y_lr_test_pred = LR(X_train, X_test, Y_train)
+        linear_regression_plot = plot_linear_regression_results(Y_train, y_lr_train_pred)
+        return linear_regression_plot
+    
+    
+
+#Function for WaterFall chart for Linear Regression
+    @output
+    @render.plot
+    def WaterfallPNG():
+        x, y = data_separation(treat)
+
+        x = pd.DataFrame(x)
+        x = x.fillna(0)
+        x = x.dropna()
+        X_train, X_test, Y_train, Y_test = data_split(x,y)
+ 
+        lr, y_lr_train_pred, y_lr_test_pred = LR(X_train, X_test, Y_train)
+        Water = create_shap_waterfall_chart(lr, x, X_test, sample_index=14, max_display=14)
+        
+        return Water
+
+    @output
+    @render.plot
+    def Random_Forest_plot():
+        x, y = data_separation(treat)
+        x = pd.DataFrame(x)
+        x = x.fillna(0)
+        x = x.dropna()
+        X_train, X_test, Y_train, Y_test = data_split(x,y)
+        rf, y_rf_train_pred, y_rf_test_pred = RF(X_train,Y_train, X_test, max_depth=2, random_state=100)
+        RF_plot = plot_RF_results(Y_train, y_rf_train_pred)
+        return RF_plot
+    
+    @output
+    @render.plot
+    def WaterRF():
+        x, y = data_separation(treat)
+        x = pd.DataFrame(x)
+        x = x.fillna(0)
+        x = x.dropna()
+        X_train, X_test, Y_train, Y_test = data_split(x,y)
+        rf, y_rf_train_pred, y_rf_test_pred = RF(X_train,Y_train, X_test, max_depth=2, random_state=100)
+        RFWater = create_shap_waterfall_chart(rf, x, X_test, sample_index=14, max_display=14)
+        return RFWater
+
+
+#Function for nav"Other Types of SHAP charts" to display two lists of the
+#positive and negative features
+    @output
+    @render.text
+    def positive_negative():
+         x, y = data_separation(treat)
+         x = pd.DataFrame(x)
+         x = x.fillna(0)
+         x = x.dropna()
+         X_train, X_test, Y_train, Y_test = data_split(x,y)
+        # Drop rows containing NaN values
+ 
+         lr, y_lr_train_pred, y_lr_test_pred = LR(X_train, X_test, Y_train)
+         positive_feature_names, negative_feature_names = Split_and_Shap(lr, x, X_test, sample_index=14, max_display=14)
+        # Return the two lists as a tuple
+         return f"Positive features : {positive_feature_names}\n Negative features :{negative_feature_names}"
+    
+#Function for nav"Other Types of SHAP charts" to display two other SHAP chart "Beeswarm" and "Violin"
+    @output
+    @render.plot
+    def plot_bee():
+        x, y = data_separation(treat)
+        x = pd.DataFrame(x)
+        x = x.fillna(0)
+        x = x.dropna()
+        X_train, X_test, Y_train, Y_test = data_split(x, y)
+        lr, y_lr_train_pred, y_lr_test_pred = LR(X_train, X_test, Y_train)
+        bee = shap_beeswarm_plot(lr, x, X_test, sample_index=14, max_display=14)
+        return bee  # Return the SHAP graph
+
+#Same but for Violin SHAP chart
+    @output
+    @render.plot
+    def plot_violin():
+        x, y = data_separation(treat)
+        x = pd.DataFrame(x)
+        x = x.fillna(0)
+        x = x.dropna()
+        X_train, X_test, Y_train, Y_test = data_split(x, y)
+        lr, y_lr_train_pred, y_lr_test_pred = LR(X_train, X_test, Y_train)
+        violin = shap_violin_plot(lr, x, X_test, sample_index=14, max_display=14)
+
+        return violin
+
+#Shiny for Python function to display Patient info on the Joblib prediction tab
+    @output
+    @render.text
+    @reactive.event(input.send3, ignore_none=False)
+    def patient_Row():
+        patient_id = input.patient_row
+        return f"Prediction for the Patient {patient_id}"
+    
+
+#Tab Joblib Prediction, Text to display Prediction made with Model and dataset
+    @output
+    @render.text
+    @reactive.event(input.send3, ignore_none=False)
+    def Pred():
+        selected_row = input.patient_row
+        selected_rows = data.iloc[selected_row() - 1]
+        prediction = Joblib(selected_rows)
+        
+        return prediction
+    
+    @output
+    @render.plot
+    @reactive.event(input.send4, ignore_none=False)
+    def Pred_plot():
+        selected_row = input.patient_row
+        plot = pred_plot(selected_row())
+        return plot
+
+
+
+#Shiny for Python for the What if navigation bar and what is inside
+    @output
+    @render.plot
+    @reactive.event(input.pred, ignore_none=False)
+    def new_LR_plt():
+        age = input.age.get()
+        blood = input.blood_pressure.get()
+        gender = input.gender
+        diabetes = input.diabetes
+        model = LinearRegression()
+        #Setting up x and y :
+        np.random.seed(19680801)
+        x = age * np.random.randn(437)
+        y = blood * np.random.randn(437)
+        X_train, X_test, Y_train, Y_test = data_split(x, y)
+        lr, y_lr_train_pred, y_lr_test_pred = LR(X_train, X_test, Y_train)
+        plot = plot_linear_regression_results(Y_train, y_lr_train_pred)
+        return plot
+
+
+
+#Shiny for Python function for the What if tab
+    @output
+    @render.plot
+    def Current():
+         #Setting up x and y :
+        x, y = data_separation(treat)
+        x = pd.DataFrame(x)
+        x = x.fillna(0)
+        x= x.dropna()
+        X_train, X_test, Y_train, Y_test = data_split(x, y)
+        lr, y_lr_train_pred, y_lr_test_pred = LR(X_train, X_test, Y_train)
+        Cur = plot_linear_regression_results(Y_train, y_lr_train_pred)
+        return Cur
+
+#Shiny for Python function to display Patient info   
+    @output
+    @render.table
+    @reactive.event(input.send, ignore_none=False)
+    def patient_table():
+        patient_id = input.patient_id
+        response = requests.get('{}/{}/{}'.format(BASE_URL, 'Patient', patient_id()))
+
+        patient_df = pd.json_normalize(response.json())[['id', 'gender', 'birthDate']]
+        patient_df = patient_df.astype({'birthDate': 'datetime64[ns]'})
+        return patient_df
+    
+
+#Shiny for Python function to display Patient history   
+    @output
+    @render.table
+    @reactive.event(input.send2, ignore_none=False)
+    def history() :
+        patient_id=input.patient_id
+        code = input.snowmed
+        response = requests.get('{}/{}?patient={}&code={}'.format(BASE_URL, 'Observation', patient_id(), code()))
+        history_df = pd.json_normalize(response.json())
+        return history_df
+
+app = App(app_ui, server)
---
+++ test.py
@@ -0,0 +1,282 @@
+import numpy as np
+import json
+import requests_fhir as requests
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
+from sklearn.linear_model import LinearRegression
+from sklearn.impute import SimpleImputer
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.ensemble import RandomForestRegressor
+import shap
+import sklearn
+import tempfile
+import seaborn as sns
+import base64 
+import io
+import os
+import joblib
+import pandas as pd
+from sklearn.feature_selection import SelectKBest, f_classif  # Use the appropriate scoring function
+from requests.exceptions import RequestException
+BASE_URL = 'https://test/fhir'
+
+
+#Function to load Patient Data
+def patient_data(patient_id):
+    try:
+        response = requests.get('{}/{}/{}'.format(BASE_URL, 'Patient', patient_id))
+        if response.status_code == 200:
+            # Normalize and select relevant columns
+            patient_df = pd.json_normalize(response.json())[['id', 'gender', 'birthDate']]
+            # Convert birthDate column to datetime
+            patient_df = patient_df.astype({'birthDate': 'datetime64[ns]'})
+            print(patient_df.info())
+            return patient_df
+        else:
+            print("Failed to fetch patient data. Status code:", response.status_code)
+            return None
+    except RequestException as e:
+        print("Error:", e)
+        return None
+
+#Function to Load Snomed CSV File
+def read_and_process_snomed_csv(file_path):
+    # Read the CSV file
+    snomed = pd.read_csv(file_path)
+    # Convert 'code' column to string type
+    snomed = snomed.astype({'code': 'string'})
+    # Display info and the first 5 rows
+    return snomed
+
+#Function to Load Treatment CSV File
+def treatment_file(file_path):
+    # Read the CSV file
+    treat = pd.read_csv(file_path)
+    # Convert 'treat_start_date' column to datetime
+    treat['treat_start_date'] = pd.to_datetime(treat['treat_start_date'])
+    # Convert 'record_id' column to string type
+    treat['record_id'] = treat['record_id'].astype(str)
+    # Display info and the DataFrame
+    print(treat.info())
+    print(treat)
+    return treat
+
+#Function for the X and Y variables
+def data_separation(treat_df):
+    y = treat_df['cvscore']
+    x = treat_df.drop(['cvscore','treat_start_date'], axis = 1)
+    return x, y
+
+#Function for Data splitting 
+def data_split(x, y, test_size=0.2, random_state=100):
+    X_train, X_test, Y_train, Y_test = train_test_split(x,y, test_size=test_size, random_state=random_state)
+    imputer = SimpleImputer(strategy='mean')
+    X_train = imputer.fit_transform(X_train)
+    X_test = imputer.fit_transform(X_test)
+    return X_train, X_test, Y_train, Y_test
+
+#Function for Linear Regression
+def LR(X_train, X_test, Y_train):
+    lr = LinearRegression()
+    lr.fit(X_train, Y_train)
+    y_lr_train_pred = lr.predict(X_train)
+    y_lr_test_pred = lr.predict(X_test)
+    return lr, y_lr_train_pred, y_lr_test_pred
+
+#Function to evaluate the Linear Regression
+def evaluate_linear_regression(Y_train, y_lr_train_pred, Y_test, y_lr_test_pred):
+    lr_train_mse = mean_squared_error(Y_train, y_lr_train_pred)
+    lr_train_r2 = r2_score(Y_train, y_lr_train_pred)
+    lr_test_mse = mean_squared_error(Y_test, y_lr_test_pred)
+    lr_test_r2 = r2_score(Y_test, y_lr_test_pred)
+    print('LR MSE (training): ', lr_train_mse)
+    print('LR R2 (training): ', lr_train_r2)
+    print('LR MSE (test): ', lr_test_mse)
+    print('LR R2 (test): ', lr_test_r2)
+    lr_result = pd.DataFrame(['Linear regression', lr_train_mse, lr_train_r2, lr_test_mse, lr_test_r2]).transpose()
+    return lr_result
+
+#Function for Random Forest
+def RF(X_train,Y_train, X_test, max_depth=2, random_state=100 ):
+    rf = RandomForestRegressor(max_depth=max_depth, random_state=random_state)
+    rf.fit(X_train, Y_train)
+    y_rf_train_pred = rf.predict(X_train)
+    y_rf_test_pred = rf.predict(X_test)
+    return rf, y_rf_train_pred, y_rf_test_pred
+
+#Function to evaluate the Random Forest 
+def train_and_evaluate_random_forest(Y_train, y_rf_train_pred, Y_test, y_rf_test_pred):
+    rf_train_mse = mean_squared_error(Y_train, y_rf_train_pred)
+    rf_train_r2 = r2_score(Y_train, y_rf_train_pred)
+    rf_test_mse = mean_squared_error(Y_test, y_rf_test_pred)
+    rf_test_r2 = r2_score(Y_test, y_rf_test_pred)
+    print('RF MSE (training): ', rf_train_mse)
+    print('RF R2 (training): ', rf_train_r2)
+    print('RF MSE (test): ', rf_test_mse)
+    print('RF R2 (test): ', rf_test_r2)
+    return rf_train_mse, rf_train_r2, rf_test_mse, rf_test_r2
+
+#Function to plot the Linear Regression
+def plot_linear_regression_results(Y_train, y_lr_train_pred):
+    z = np.polyfit(Y_train, y_lr_train_pred, 1)
+    p = np.poly1d(z)
+    scatter = sns.scatterplot(x=Y_train, y=y_lr_train_pred, alpha=0.3)
+    plt.plot(Y_train, p(Y_train), color='red', linewidth=2)
+    plt.xlabel('Actual CVS Score')
+    plt.ylabel('Predicted CVS Score')
+    plt.title('Linear Regression: Actual vs. Predicted CVS Score')
+
+#Function to plot the Linear Regression
+def plot_RF_results(Y_train, y_rf_train_pred):
+    plt.figure(figsize=(5, 5))
+    plt.scatter(x=Y_train, y=y_rf_train_pred, alpha=0.3)
+    z = np.polyfit(Y_train, y_rf_train_pred, 1)
+    p = np.poly1d(z)
+    plt.plot(Y_train, p(Y_train), color='red')
+    plt.xlabel('Actual CVS Score')
+    plt.ylabel('Predicted CVS Score')
+    plt.title('Random Forest: Actual vs. Predicted CVS Score')
+
+#Function to plot a SHAP Waterfall chart
+def create_shap_waterfall_chart(model, x, X_test, sample_index=14, max_display=14):
+    # Get feature names from the DataFrame
+    feature_names = x.columns.tolist()
+    # Create a SHAP explainer
+    explainer = shap.Explainer(model, X_test, feature_names=feature_names)
+    # Calculate SHAP values
+    shap_values = explainer(X_test)
+    # Create a SHAP waterfall chart for a specific instance
+    shap.plots.waterfall(shap_values[sample_index], max_display=max_display, show=False)
+
+
+#Function to plot a BeeSwarm SHAP chart
+def shap_beeswarm_plot(model, x, X_test, sample_index, max_display=20):
+    # Create a SHAP beeswarm plot
+    feature_names = x.columns.tolist()
+    explainer = shap.Explainer(model, X_test, feature_names=feature_names)
+    shap_values = explainer(X_test)
+    shap.plots.beeswarm(shap_values, max_display=max_display, show=False)
+ 
+
+
+#Function to plot a Violin SHAP chart
+def shap_violin_plot(model, x, X_test, sample_index, max_display=20):
+    # Create a SHAP violin plot
+    feature_names = x.columns.tolist()
+    explainer = shap.Explainer(model, X_test, feature_names=feature_names)
+    shap_values = explainer(X_test)
+    shap.plots.violin(shap_values, show=False)
+
+
+
+#Function for the Shap chart nav to create both the Beeswarm and Violin chart
+#and to  retrieve the positive and negative features in a list
+def Split_and_Shap(model, x, X_test, sample_index, max_display=14):
+    # Get feature names from the DataFrame
+    feature_names = x.columns.tolist()
+    
+    # Create a SHAP explainer
+    explainer = shap.Explainer(model, X_test, feature_names=feature_names)
+    
+    # Calculate SHAP values
+    shap_values = explainer(X_test)
+    
+    # Create two SHAP charts so that we can retrieve the features
+    shap.plots.beeswarm(shap_values, max_display=max_display, show=False)
+    shap.plots.violin(shap_values, show=False)
+
+    # Initialize empty lists for positive and negative feature names
+    positive_feature_names = []
+    negative_feature_names = []
+
+    # Iterate through the SHAP values and split them
+    for i, value in enumerate(shap_values):
+        if i < len(feature_names):  # Ensure that the index is within the bounds of feature_names
+            feature_name = feature_names[i]  # Get the feature name from the corresponding index
+
+            if any(value.values > 0):  # Check if any value in the Explanation is positive
+                positive_feature_names.append(feature_name)
+            elif any(value.values < 0):  # Check if any value in the Explanation is negative
+                negative_feature_names.append(feature_name)
+
+    # Return positive and negative feature names as two separate lists
+    return positive_feature_names, negative_feature_names
+    
+#Function to display and create a Linear Regression graph and save it as an image
+def create_and_display_graph_test(Y_train, y_lr_train_pred, image_path):
+    z = np.polyfit(Y_train, y_lr_train_pred, 1)
+    p = np.poly1d(z)
+    scatter = sns.scatterplot(x=Y_train, y=y_lr_train_pred, alpha=0.3)
+    plt.plot(Y_train, p(Y_train), color='red', linewidth=2)
+    plt.xlabel('Actual CVS Score')
+    plt.ylabel('Predicted CVS Score')
+    plt.title('Linear Regression: Actual vs. Predicted CVS Score')
+    # Create a temporary directory
+    #temp_dir = tempfile.mkdtemp()
+    # Define the path for the saved image
+    #image_path = f"{temp_dir}/sample_plot.png"
+    # Save the plot as an image
+    plt.savefig(image_path, bbox_inches="tight")
+    # Display the plot
+    plt.show()
+    # Close the plot to release resources
+    plt.close()
+
+
+
+#Function for the Joblib Method and to realize predictions for each patients
+def Joblib(selected_row):
+ # Load the pre-trained model
+    model = joblib.load('model.joblib')
+
+    # Read the CSV file into a DataFrame
+    data = pd.read_csv('simulated_data.csv')
+#KE
+    # Define the number of features to select (304 in your case)
+    num_features_to_select = 304
+
+    # Extract the selected patient's data
+    patient_data = data.iloc[selected_row - 1]  # Assuming selected_row is the row number you want to predict
+
+    # Extract the 'days_from_diag' column from the patient's data
+    days_from_diag = patient_data['days_from_diag']
+
+    # Find the index (position) of the 'days_from_diag' column
+    days_from_diag_idx = data.columns.get_loc('days_from_diag')
+
+    # Extract features starting from 'days_from_diag' and select the next 304 columns
+    selected_features = patient_data[days_from_diag_idx + 1:days_from_diag_idx + num_features_to_select + 1]
+
+    # Create a new DataFrame with the selected features
+    selected_data = pd.DataFrame(selected_features).T
+
+    # Make predictions
+    prediction = model.predict(selected_data)
+    per = round(prediction[0] * 100, 2)
+
+    return f"The risk of dying within 30 days for patient {selected_row} is {per:.2f}% according to my model."
+
+from io import BytesIO
+
+def pred_plot(selected_row):
+    # Load the pre-trained model
+    model = joblib.load('model.joblib')
+    # Read the CSV file into a DataFrame
+    data = pd.read_csv('simulated_data.csv')
+
+    # Extract the selected patient's data
+    patient_data = data.iloc[selected_row - 1]
+    days_from_diag = patient_data['days_from_diag'].tolist()
+
+    # Create a bar plot to visualize the 'days_from_diag' values
+    plt.figure(figsize=(8, 6))
+    plt.bar(days_from_diag, days_from_diag)
+    plt.xlabel('Feature Index')
+    plt.ylabel('Days from Diagnosis')
+    plt.title(f'Days from Diagnosis for Patient {selected_row}')
